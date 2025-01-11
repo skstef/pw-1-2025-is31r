dcc16c483baa251ebd6b3a862255838ab52ab3c8
skstef <sstefstefv@gmail.com>
2025-01-11 12:11:00 +0200

    refactor: rename folder

--- 10-01-2025/task2.js
+++
@@ -1,8 +0,0 @@
-function uniqueSorted(arr) {
-    return [...new Set(arr)].sort((a, b) => a - b);
-}
-
-const arr = [8, 7, 2, 2, 3, 4, 2, 5, 5, 6, 7, 8, 5, 9, 0, 1];
-
-const result = uniqueSorted(arr);
-console.log(result);
