5c49c645820bc199ab4c7c00981739a5cf458372
skstef <sstefstefv@gmail.com>
2025-05-20 13:31:48 +0300

    feature: EP task 5

---
+++ ep-20.05.2025/task_5/index.js
@@ -0,0 +1,7 @@
+function getObjectKeys(obj) {
+  return Object.keys(obj);
+}
+
+// Usage examples
+console.log(getObjectKeys({ name: "Alice", age: 25 })); // Output: ['name', 'age']
+console.log(getObjectKeys({ a: 1, b: 2, c: 3 })); // Output: ['a', 'b', 'c']
